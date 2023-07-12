f4334abff0082a2db54dab27379ac62dfd81112f
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2023-07-12 13:35:58 +0700

    Add function

--- function/__init__.py
+++ function/__init__.py
@@ -11,19 +11,40 @@ def main(msg: func.ServiceBusMessage):
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
 
-    # TODO: Get connection to database
-
+    conn = psycopg2.connect("dbname=test user=postgres")
     try:
-        # TODO: Get notification message and subject from database using the notification_id
+        cur = conn.cursor()
 
-        # TODO: Get attendees email and name
+        cur.execute("SELECT message, subject FROM notification WHERE id = %s;",(notification_id,))
+        messagePlain, subject = cur.fetchone()
 
-        # TODO: Loop through each attendee and send an email with a personalized subject
+        cur.execute("SELECT email, first_name FROM attendee;")
+        attendees = cur.fetchall()
 
-        # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
-        ...
+        for attendee in attendees:
+            message = Mail(
+                from_email='from_email@example.com',
+                to_emails=attendee[0],
+                subject=f'{attendee[1]}: {subject}',
+                html_content=messagePlain)
+
+            try:
+                sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+                response = sg.send(message)
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+
+            except Exception as e:
+                print(str(e))
+
+        # Update the notification table by setting the completed date and updating the status with the total number of attendees notified
+        total_attendees = 'Notified {} attendees'.format(len(attendees))
+        cur.execute("UPDATE notification SET status = %s WHERE id = %s;", (total_attendees,notification_id))
+        conn.commit()
+
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
+
     finally:
-        # TODO: Close connection
-        ...
+        conn.close()
